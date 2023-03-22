6da2cf61a488cd4c438274cb5579863972342ac4
soyoungbaekenny <107281116+soyoungbaekenny@users.noreply.github.com>
2023-03-22 16:40:09 +1000

    Add files via upload

---
+++ Data_Processing_1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import csv
+# df = pd.concat(map(pd.read_csv, ['daily_sales_data_0.csv', 'daily_sales_data_1.csv', 'daily_sales_data_2.csv']),ignore_index=True)
+# print(df)
+# #df = pd.read_csv("daily_sales_data_0.csv")
+# pink_morsel_only = df[(df['product'] == 'pink morsel')]
+# print(pink_morsel_only)
+# pink_morsel_only.to_csv('Pink_Morsel_Sales.csv')
+
+
+# df = pd.read_csv("Pink_Morsel_Sales.csv")
+# price = df['price'].replace('[\$,]', '', regex=True)
+# df['price'] = price
+# df['sales'] = df['price'].astype(float) * df['quantity'].astype(float)
+# print(df)
+# df.to_csv('sales_per_date.csv')
+
+df = pd.read_csv("sales_per_date.csv")
+new_frame = (df[['sales', 'date', 'region']])
+print(new_frame.sort_values(['region', 'date'], ascending=[False,True]))
+df.to_csv('final.csv')
---
+++ Data_visualisation.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from dash import Dash, html, dcc
+
+from plotly.express import line
+
+csv_file = "./final.csv"
+
+df = pd.read_csv(csv_file)
+df = df.sort_values(by="date")
+app = Dash(__name__)
+
+fig = line(df, x="date", y="sales", title="Pink Morsel Sales")
+visualization = dcc.Graph(
+    id="visualization",
+    figure=fig
+)
+app.layout = html.Div(children=[
+    html.H1(children='Pink Morsels Sales Analysis - Soul Food'),
+    html.Div(children='''Sales analysis to and from price increase on 15th Jan.'''),
+    dcc.Graph(id='example-graph', figure=fig)
+])
+
+if __name__ == '__main__':
+    app.run_server()
